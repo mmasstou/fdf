94580a30e174a755f4f9b15d13d0bce888632651
mohamed MASSTOUR <mmasstou@student.1337.ma>
2022-02-20 11:48:49 +0100

    add colors logique and function

--- srcs/ft_colors.c
+++ srcs/ft_colors.c
@@ -1,9 +1,48 @@
 #include "../incs/fdf.h"
 
-int	ft_colors(int altitude)
+int	rgb_percent(int p)
 {
-	int	color;
+	return ((255 * p) / 100);
+}
 
-	
-	return (color);
+void	ft_puthex(unsigned int nbr, char *hex)
+{
+	int i;
+
+	if (nbr > 16)
+	{
+		ft_puthex(nbr / 16, hex);
+		ft_puthex(nbr % 16, hex);
+	}
+	else
+	{
+		i  = 0;
+		if (nbr > 10)
+			hex[i] = nbr + 48 ;
+		else 
+			hex[i] = (nbr - 10) + 'a';
+		i++;
+		hex[i] = '\0';
+	}
+}
+
+int	ft_colors(t_color color)
+{
+	char *hex;
+	char *hexc;
+	int r;
+	int g;
+	int b;
+
+	r = rgb_percent(color.r);
+	g = rgb_percent(color.g);
+	b = rgb_percent(color.b);
+	ft_puthex(r, hexc);
+	hex = ft_strdup(hexc);  // r
+	ft_puthex(g, hexc);
+	hex = ft_strjoin(hex, hexc); // g
+	ft_puthex(b, hexc);
+	hex = ft_strjoin(hex, hexc);  // b
+
+	return (ft_hextoi(hex));
 }

3a516f03a707d7efa7411baa6c18291cb1e3bb86
mohamed MASSTOUR <mmasstou@student.1337.ma>
2022-02-21 12:37:29 +0100

    norm srcs/draw.c: OK

--- srcs/draw.c
+++ srcs/draw.c
@@ -1,108 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmasstou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 12:36:47 by mmasstou          #+#    #+#             */
+/*   Updated: 2022/02/21 12:36:50 by mmasstou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../incs/fdf.h"
 
-int ft_color(float z, float z1 ,fdf *fdf)
+static void	isomitric_fdf(float *x, float *y, float z, fdf *data)
 {
-	int color;
-	(void)fdf;
-	
-	if (fdf->key == RANDOM_COLORS)
-		color = 0xFFFFFF  * fabsf( fdf->zom / fdf->altitude);
-	else if (fdf->key == DEFAULT_COLORS)
-		color = 0xFFFFFF;
-	else 
-		color = 0xFFFFFF;
-	if (z || z1)
-		return(color);
-	else
-		return(color);
+	rotation(x, y, z, data);
+	*x = (*x - *y) * cos(data->beta);
+	*y = -z + (*x + *y) * sin(data->beta);
 }
 
-int ft_close(int key, fdf *param)
+void	m_zoom(fdf *fdf)
 {
-	if (key == 53)
-    {
-		mlx_destroy_window(param->img->mlx, param->img->win);
-		exit(0);
-	}
-	return (0);
-}
-void	ft_drawline(fdf *map, int x, int y, int color)
-{
-	char	*dst;
-
-	if((x >= 0 && x < WIDTH) && (y >= 0 && y < HEIGHT))
-	{
-		dst = map->img->img_addr + ( y * map->img->line_len + x * (map->img->bpp / 8));
-		*(unsigned int *)dst = color;
-	}
+	fdf->pixel->x2 *= fdf->zom;
+	fdf->pixel->y2 *= fdf->zom;
+	fdf->pixel->x1 *= fdf->zom;
+	fdf->pixel->y1 *= fdf->zom;
+	fdf->pixel->z *= fdf->altitude;
+	fdf->pixel->z1 *= fdf->altitude;
+	isomitric_fdf(&fdf->pixel->x1, &fdf->pixel->y1, fdf->pixel->z, fdf);
+	isomitric_fdf(&fdf->pixel->x2, &fdf->pixel->y2, fdf->pixel->z1, fdf);
+	fdf->pixel->x1 += fdf->pad_w;
+	fdf->pixel->y1 += fdf->pad_h;
+	fdf->pixel->x2 += fdf->pad_w;
+	fdf->pixel->y2 += fdf->pad_h;
 }
 
-static void isomitric_fdf(float *x, float *y, float z , fdf *data)
+void	dda(fdf *fdf)
 {
-	if (data->key == RO_X_UP || data->key == RO_X_DOWN)
-	{
-		z = -*y * sin(data->alpha) + z * cos(data->alpha);
-		*y = *y * cos(data->alpha) + z * sin(data->alpha);
-	}
-	else if (data->key == RO_Y_UP || data->key == RO_Y_DOWN)
-	{
-		z = -*x * sin(data->alpha) + z * cos(data->alpha);
-		*x = *x * cos(data->alpha) + z * sin(data->alpha);
-	}
-	else if (data->key == RO_Z_UP || data->key == RO_Z_DOWN)
-	{
-		*x = *x * cos(data->alpha) + *y * sin(data->alpha);
-		*y = *y * cos(data->alpha) + *x * sin(data->alpha);
-	}
-    *x = (*x - *y) * cos(data->beta);
-    *y = -z + (*x + *y) * sin(data->beta);
-}
-
-void	dda(float x1, float y1, float x2, float y2, fdf *fdf)
-{
-	float	dx;
-	float	dy;
-	int		max;
+	float	d[2];
 	int		color;
-	float		z;
-	float		z1;
-	
-	color = fdf->matrix[(int)y1][(int)x1].color;
-	z = fdf->matrix[(int)y1][(int)x1].z;
-	z1 = fdf->matrix[(int)y2][(int)x2].z;
-	
+	int		max;
+
+	color = fdf->pixel->color;
 	if (color == -1 || color == 0xffffff)
-		color = ft_color(z, z1,fdf);
-	x1 *= fdf->zom;
-	y1 *= fdf->zom;
-	x2 *= fdf->zom;
-	y2 *= fdf->zom;
-
-
-
-
-	z *= fdf->altitude;
-	z1 *= fdf->altitude;
-	isomitric_fdf(&x1, &y1, z, fdf);
-	isomitric_fdf(&x2, &y2, z1, fdf);
-
-	x1 += fdf->pad_w;
-	y1 += fdf->pad_h;
-	x2 += fdf->pad_w;
-	y2 += fdf->pad_h;
-	dx = x2 - x1;
-	dy = y2 - y1;
-
-	max = fmax(fabs(dx), fabs(dy));
-	dx /= max;
-	dy /= max;
-	
-	while ((int)(x1 - x2) || (int)(y1 - y2))
+		color = ft_color(fdf->pixel->z, fdf->pixel->z1, fdf);
+	m_zoom(fdf);
+	d[0] = fdf->pixel->x2 - fdf->pixel->x1;
+	d[1] = fdf->pixel->y2 - fdf->pixel->y1;
+	max = fmax(fabs(d[0]), fabs(d[1]));
+	d[0] /= max;
+	d[1] /= max;
+	while ((int)(fdf->pixel->x1 - fdf->pixel->x2) || \
+	(int)(fdf->pixel->y1 - fdf->pixel->y2))
 	{
-		ft_drawline(fdf, x1, y1, color);
-		x1 += dx;
-		y1 += dy;
+		ft_drawline(fdf, fdf->pixel->x1, fdf->pixel->y1, color);
+		fdf->pixel->x1 += d[0];
+		fdf->pixel->y1 += d[1];
 	}
+}
+
+void	pixel_manage(int x, int y, fdf *fdf, char c)
+{
+	fdf->pixel->x1 = x;
+	fdf->pixel->y1 = y;
+	fdf->pixel->x2 = x;
+	fdf->pixel->y2 = y;
+	if (c == 'x')
+		fdf->pixel->x2 = x + 1;
+	if (c == 'y')
+		fdf->pixel->y2 = y + 1;
+	fdf->pixel->z = fdf->matrix[(int)y][(int)x].z;
+	fdf->pixel->z1 = fdf->matrix[(int)fdf->pixel->y2][(int)fdf->pixel->x2].z;
+	fdf->pixel->color = fdf->matrix[(int)y][(int)x].color;
 }
 
 void	draw_map(fdf *fdf)
@@ -117,12 +86,17 @@ void	draw_map(fdf *fdf)
 		while (y < fdf->height)
 		{
 			if (x < fdf->width - 1)
-				dda(x, y, x + 1, y, fdf);
+			{
+				pixel_manage(x, y, fdf, 'x');
+				dda(fdf);
+			}
 			if (y < fdf->height - 1)
-				dda(x, y, x, y + 1, fdf);
+			{
+				pixel_manage(x, y, fdf, 'y');
+				dda(fdf);
+			}
 			y++;
 		}
 		x++;
 	}
 }
-

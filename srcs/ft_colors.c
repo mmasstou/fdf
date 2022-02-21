c053f08a7bcd202fb621ccaec500a85f240b24c9
mohamed MASSTOUR <mmasstou@student.1337.ma>
2022-02-21 09:53:05 +0100

    norm srcs/ft_colors.c: OK

--- srcs/ft_colors.c
+++ srcs/ft_colors.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_colors.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmasstou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 09:52:01 by mmasstou          #+#    #+#             */
+/*   Updated: 2022/02/21 09:52:05 by mmasstou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../incs/fdf.h"
 
 static int	rgb_percent(int p)
@@ -5,9 +17,8 @@ static int	rgb_percent(int p)
 	return ((255 * p) / 100);
 }
 
-static void ft_puthex(unsigned int nbr , char *tab, int *i)
+static void	ft_puthex(unsigned int nbr, char *tab, int *i)
 {
-
 	if (nbr > 16)
 	{
 		ft_puthex(nbr / 16, tab, i);
@@ -20,7 +31,7 @@ static void ft_puthex(unsigned int nbr , char *tab, int *i)
 			tab[*i] = nbr + 48;
 			*i += 1;
 		}
-		else 
+		else
 		{
 			tab[*i] = (nbr - 10) + 'a';
 			*i += 1;
@@ -28,13 +39,13 @@ static void ft_puthex(unsigned int nbr , char *tab, int *i)
 	}
 }
 
-int ft_rgb(int per_r, int per_g, int per_b)
+int	ft_rgb(int per_r, int per_g, int per_b)
 {
-	char *hexc;
-	int index;
-	int red;
-	int green;
-	int blue;
+	char	*hexc;
+	int		index;
+	int		red;
+	int		green;
+	int		blue;
 
 	index = 0;
 	hexc = (char *)malloc(14);
@@ -43,11 +54,42 @@ int ft_rgb(int per_r, int per_g, int per_b)
 	red = rgb_percent(per_r);
 	green = rgb_percent(per_g);
 	blue = rgb_percent(per_b);
-	ft_puthex(red,hexc,&index);
-	ft_puthex(green,hexc,&index);
-	ft_puthex(blue,hexc,&index);
-	hexc = ft_strjoin("0x",hexc);
+	ft_puthex(red, hexc, &index);
+	ft_puthex(green, hexc, &index);
+	ft_puthex(blue, hexc, &index);
+	hexc = ft_strjoin("0x", hexc);
 	index = ft_hextoi(hexc);
 	free(hexc);
 	return (index);
+}
+
+void	colors(int key, fdf *data)
+{
+	if (key == RANDOM_COLORS)
+	{
+		data->colos_name = ft_strdup("Random");
+		data->key = RANDOM_COLORS;
+	}
+	if (key == DEFAULT_COLORS)
+	{
+		data->colos_name = ft_strdup("Default");
+		data->key = DEFAULT_COLORS;
+	}
+}
+
+int	ft_color(float z, float z1, fdf *fdf)
+{
+	int	color;
+
+	(void)fdf;
+	if (fdf->key == RANDOM_COLORS)
+		color = 0xFFFFFF * fabsf(fdf->zom / fdf->altitude);
+	else if (fdf->key == DEFAULT_COLORS)
+		color = 0xFFFFFF;
+	else
+		color = 0xFFFFFF;
+	if (z || z1)
+		return (color);
+	else
+		return (color);
 }

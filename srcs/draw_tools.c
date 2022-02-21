a6a9c61ec5db02a0ebfd1059ecf2e710bfeb989c
mohamed MASSTOUR <mmasstou@student.1337.ma>
2022-02-21 12:39:59 +0100

    norm srcs/draw_tools.c: OK

--- srcs/draw_tools.c
+++ srcs/draw_tools.c
@@ -27,3 +27,29 @@ char	*ft_title(char *title)
 	}
 	return (split[0]);
 }
+
+int	re_draw(fdf *data)
+{
+	mlx_clear_window(data->img->mlx, data->img->win);
+	mlx_destroy_image(data->img->mlx, data->img->img);
+	data->img->img = mlx_new_image(data->img->mlx, WIDTH, HEIGHT);
+	data->img->img_addr = mlx_get_data_addr(data->img->img, \
+	&data->img->bpp, &data->img->line_len, &data->img->endian);
+	draw_map(data);
+	mlx_put_image_to_window(data->img->mlx, \
+	data->img->win, data->img->img, 0, 0);
+	print_menu(data);
+	return (0);
+}
+
+void	ft_drawline(fdf *map, int x, int y, int color)
+{
+	char	*dst;
+
+	if ((x >= 0 && x < WIDTH) && (y >= 0 && y < HEIGHT))
+	{
+		dst = map->img->img_addr + (y * \
+		map->img->line_len + x * (map->img->bpp / 8));
+		*(unsigned int *)dst = color;
+	}
+}

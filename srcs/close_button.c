2acbe0fb5a488a9ba2b4e89f315bbb2724400123
mohamed MASSTOUR <mmasstou@student.1337.ma>
2022-02-25 12:50:43 +0100

    project is done

--- srcs/close_button.c
+++ srcs/close_button.c
@@ -6,7 +6,7 @@
 /*   By: mmasstou <mmasstou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 15:17:44 by mmasstou          #+#    #+#             */
-/*   Updated: 2022/02/24 15:17:45 by mmasstou         ###   ########.fr       */
+/*   Updated: 2022/02/25 12:49:59 by mmasstou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	x_button(t_fdf *fdf)
 {
 	(void)fdf;
-	exit(0);
+	exit(EXIT_SUCCESS);
 }
 
 int	ft_close(int key, t_fdf *fdf)
@@ -23,7 +23,7 @@ int	ft_close(int key, t_fdf *fdf)
 	if (key == 53)
 	{
 		mlx_destroy_window(fdf->img->mlx, fdf->img->win);
-		exit(0);
+		exit(EXIT_SUCCESS);
 	}
 	return (0);
 }
